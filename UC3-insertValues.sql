39c2a8db2926758ff46e3a71ce0b6a134afe4d81
Mansi-Atey <mansiatey31@gmail.com>
2021-04-11 17:11:19 +0530

    [mansi]ADDED: query to insert values in table

---
+++ UC3-insertValues.sql
@@ -0,0 +1,6 @@
+ insert into [dbo].[address_book] values
+('Mansi', 'Atey', 'Andheri', 'Mumbai', 'Maharashtra', 400058, '9459450954', 'manu@gmail.com'),
+('Neha', 'Adhav', 'KGN Society', 'Nagpur', 'Maharashtra', 422333, '7443443434', 'neha@gmail.com'),
+('Shreyas', 'Iyer', 'Bandra', 'Mumbai', 'Maharashtra', 400033, '8889454954', 'shreyas@gmail.com'),
+('Rohan', 'Mehra', 'Shivaji Nagar ', 'Pune', 'Maharashtra', 420033, '8458765954', 'rohan@gmail.com')
+select * from address_book
