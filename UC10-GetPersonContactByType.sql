25ed6bd9141e098b0aa5bd8b34b5d80e183e44d9
Mansi-Atey <mansiatey31@gmail.com>
2021-04-13 01:22:38 +0530

    [mansi]ADDED: ability to get number of contact person

---
+++ UC10-GetPersonContactByType.sql
@@ -0,0 +1,5 @@
+select count(addressbook_type) from [dbo].[address_book] where addressbook_type = 'Family';
+
+select count(addressbook_type) from [dbo].[address_book] where addressbook_type = 'Friends';
+
+select count(addressbook_type) from [dbo].[address_book] where addressbook_type = 'Profession';
